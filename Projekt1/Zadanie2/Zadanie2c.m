1966d6fa6701416ef2383c0246014f8ab911fa07
Maciej Suchocki <m.suchocki96@gmail.com>
2018-03-23 20:10:43 +0100

    forward and backward substitution used in (b) and (c)

--- Projekt1/Zadanie2/Zadanie2c.m
+++ Projekt1/Zadanie2/Zadanie2c.m
@@ -2,7 +2,7 @@ function [] = Zadanie2c(n)
     A = c_genA(n);
     b = c_genB(n);
     L1 = cholesky(A,b,n);
-    x = backwardSubstitution(L1,b);
+    x = solveEq(L1,b);
     fprintf('Odpowiedz na zadanie to: \n');
     disp(x);
 end
